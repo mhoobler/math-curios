da69987028bf55029eb2f90240ca156d1ccfd45b
mhoobler <hoobler.michael@gmail.com>
2021-03-21 21:31:09 -0400

    added project

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = __dirname + "/public/projects/circle-derivative";
 const path2 = __dirname + "/public/projects/repulsive";
+const path3 = __dirname + "/public/projects/left-shift";
 
 app.use(express.static("./public"));
 app.use("/circle-derivative", express.static(path));
 app.use("/repulsive", express.static(path2));
+app.use("/left-shift", express.static(path3));
 
 app.get("*", (req, res) => {
   console.log(req.originalUrl);
